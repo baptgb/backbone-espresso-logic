2ed632dde6389a9b054d452df664139daf02c477
Baptiste Gouby <baptiste.gouby@gmail.com>
2014-08-09 17:33:18 -0400

    Added Collection and comments

--- backbone.espressologic.js
+++ backbone.espressologic.js
@@ -5,11 +5,19 @@ Backbone.EspressoLogic = (function(Backbone, _) {
 
     var EspressoLogic = {};
     EspressoLogic.Model = Backbone.Model.extend({
+        /**
+         * Using Espresso Logic, we can provide SQL-like filters to the requested URL
+         * We just have to set a filter in the options when creating a model
+         */
         initialize: function(attr, opts) {
             if (opts && opts.apiFilter) {
                 this.apiFilter = opts.apiFilter;
             }
         },
+        /**
+         * The URL used to make the request is created.
+         * Append the filter to the URL
+         */
         url: function() {
             var url = this.urlRoot;
             if (this.apiFilter) {
@@ -21,13 +29,28 @@ Backbone.EspressoLogic = (function(Backbone, _) {
             // If the response has a status code 201 (Created), the Model data object will be the only item in the 'txsummary' array
             if (response.statusCode && response.statusCode === 201) {
                 return response.txsummary[0];
-            // Espresso Logic returns an array even if we GET a signle entity like '[...]/user/1
+            // Espresso Logic returns an array even if we GET a single entity like '[...]/user/1
             // If the array length is only 1, it returns object
             } else if (response.length === 1) {
                 return response[0];
             } else {
                 return response;
             }
+        }
+    });
+
+    EspressoLogic.Collection = Backbone.Collection.extend({
+        initialize: function(attr, opts) {
+            if (opts && opts.apiFilter) {
+                this.apiFilter = opts.apiFilter;
+            }
+        },
+        url: function() {
+            var url = this.urlRoot;
+            if (this.apiFilter) {
+                url += this.apiFilter;
+            }
+            return url;
         }
     });
 
