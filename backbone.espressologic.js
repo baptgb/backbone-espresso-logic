c604846ecc172f4c9d99fe0c10c3e4c3d78c21d1
Baptiste Gouby <baptiste.gouby@gmail.com>
2014-08-26 21:45:23 -0400

    Added HTTP 200 support

--- backbone.espressologic.js
+++ backbone.espressologic.js
@@ -26,8 +26,8 @@ Backbone.EspressoLogic = (function(Backbone, _) {
             return url;
         },
         parse: function(response) {
-            // If the response has a status code 201 (Created), the Model data object will be the only item in the 'txsummary' array
-            if (response.statusCode && response.statusCode === 201) {
+            // If the response has a status code 201 (Created) or 200 (OK), the Model data object will be the only item in the 'txsummary' array
+            if (response.statusCode && (response.statusCode === 201 || response.statusCode === 200)) {
                 return response.txsummary[0];
             // Espresso Logic returns an array even if we GET a single entity like '[...]/user/1
             // If the array length is only 1, it returns object
